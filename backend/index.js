e58d058baa7797fac44fd6cf5eb1dc43da8e436f
yuvraj <singhyuvraj0717@gmail.com>
2025-05-24 15:59:01 +0530

    remove .env file

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,10 @@
 const express = require("express");
 const app = express();
-require('dotenv').config();
 
 const connectDB = require('./src/database/db');
 
 app.use(express.json());
 connectDB();
-
-app.get("/", (req, res) => {
-  res.send("Todo backend running ✅");
-});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
