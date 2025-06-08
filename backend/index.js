466aa93a85d0ff7d3d8c034ad57781ea3bdf0fb5
yuvraj <singhyuvraj0717@gmail.com>
2025-06-08 15:15:14 +0530

    final commit

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import path from "path";
 
 import { connectDB } from "./src/lib/db.js";
 
@@ -11,6 +12,8 @@ import authRoutes from './src/routes/auth.route.js'
 dotenv.config();
 
 const app = express();
+
+const __dirname = path.resolve();
 
 app.use(express.json());
 connectDB();
@@ -23,6 +26,14 @@ app.use(
 );
 
 app.use('/api/auth', authRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
