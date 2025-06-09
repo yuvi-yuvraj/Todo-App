709f7f04e00ddd0abc89feaa9646fdcb5309e692
yuvraj <singhyuvraj0717@gmail.com>
2025-06-09 18:23:27 +0530

    add changes

--- backend/src/middleware/authMid.js
+++ backend/src/middleware/authMid.js
@@ -1,31 +1,23 @@
 import jwt from "jsonwebtoken";
-import User from "../models/user.model.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export const protectRoute = async (req, res, next) => {
-  try {
-    const token = req.cookies.jwt;
+  const token = req.headers?.authorization?.split(" ")[1];
+    if(token){
+        const decoded = jwt.verify(token,process.env.JWT_KEY);
+        if(decoded){
+            const userID = decoded.userID;
+            req.body.userID = userID;
+            next ( );
+        }
 
-    if (!token) {
-      return res.status(401).json({ message: "Unauthorized - No Token Provided" });
+        else{
+            res.status(400).send({"message" : "User Not Found, Try Logging In"})
+        }
     }
-
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
-    if (!decoded) {
-      return res.status(401).json({ message: "Unauthorized - Invalid Token" });
+    else{
+        res.status(400).send({"message" : "User Not Found, Try Logging In"})
     }
-
-    const user = await User.findById(decoded.userId).select("-password");
-
-    if (!user) {
-      return res.status(404).json({ message: "User not found" });
-    }
-
-    req.user = user;
-
-    next();
-  } catch (error) {
-    console.log("Error in protectRoute middleware: ", error.message);
-    res.status(500).json({ message: "Internal server error" });
-  }
 };
