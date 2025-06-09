9a623533b6cfc1d832241c2fa48e3e1fd3c78c0f
yuvraj <singhyuvraj0717@gmail.com>
2025-06-09 18:25:27 +0530

    add changes

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "/api",
+    baseURL: "http://localhost:5000/api",
     withCredentials: true,
 });
